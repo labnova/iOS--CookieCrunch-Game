bdadf9c4600f39c23337e650e03e2104829efdba
labnova <progettofeb@gmail.com>
2015-03-15 16:25:44 +0100

    add class and enum "Cookie"

---
+++ CookieCrunch/Cookie.swift
@@ -0,0 +1,57 @@
+//
+//  Cookie.swift
+//  CookieCrunch
+//
+//  Created by Innocenzo Tremamondo on 15/03/15.
+//  Copyright (c) 2015 LabNova. All rights reserved.
+//
+
+import SpriteKit
+
+enum CookieType: Int, Printable {
+    case Unkown = 0, Croissant, Cupcake, Danish, Donut, Macaroon, SugarCookie
+    
+    var spriteName: String {
+        let spriteNames = [
+            "Croissant",
+            "Cupcake",
+            "Danish",
+            "Donut",
+            "Macaroon",
+            "SugarCookie" ]
+        
+        return spriteNames[rawValue - 1]
+    }
+    
+    var hightlightedSpriteName: String {
+        return spriteName + "-Highlighted"
+    }
+    
+    static func random() -> CookieType {
+        return CookieType(rawValue: Int(arc4random_uniform(6)) + 1)!
+    }
+    
+    var description: String {
+        return spriteName
+    }
+    
+}
+
+class Cookie : Printable {
+    var column: Int
+    var row: Int
+    let cookieType: CookieType
+    var sprite: SKSpriteNode?
+    
+    var description:String {
+        return "type: \(cookieType) square:(\(column), \(row))"
+    }
+    
+    init(column: Int, row: Int, cookieType: CookieType) {
+        self.column = column
+        self.row = row
+        self.cookieType = cookieType
+    }
+    
+    
+}
